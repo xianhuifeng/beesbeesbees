c22b30e729348cb3068eb517c1de8294dfb3f13d
lexjacobs <lex.jacobs@gmail.com>
2014-06-09 20:13:16 -0700

    Sets last test to be consistent with test description.

--- test/Bee.js
+++ test/Bee.js
@@ -29,7 +29,7 @@ describe('Bee class functionality', function() {
   /*  New methods and properties  */
 
   it('should have a `job` property that is set to `keep on growing`', function() {
-    expect(bee.eat).to.be.a('function');
+    expect(bee.job).to.equal('keep on growing');
   });
 
 });
