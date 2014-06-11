257b874d93c8a84844822f9a492b25b653ab1388
Hack Reactor Students <xianhui.feng100@gmail.com>
2014-06-10 20:16:19 -0700

    finished

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forrage = function(){
+  this.treasureChest.push('treasure');
+};
